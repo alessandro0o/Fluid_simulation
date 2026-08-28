11a3efbf6a62ffe29dbaff14bea38ce0c19567cc
alessandro0o <alessandroruspi09@gmail.com>
2026-08-28 11:49:01 +0200

    Update Color_coding.h

--- Color_coding.h
+++ Color_coding.h
@@ -3,29 +3,40 @@
 
 Color getColor(float value)
 {
-    const int gray = 128;
-
-    float v = value / 5.0f;
-    if (v < -1.0f) v = -1.0f;
-    if (v > 1.0f) v = 1.0f;
-
     Color c;
 
-    if (v < 0.0f)
-    {
-        float t = -v;
-        c.r = gray + static_cast<int>((255 - gray) * t);
-        c.g = gray - static_cast<int>(gray * t);
-        c.b = gray - static_cast<int>(gray * t);
-    }
-    else
-    {
-        float t = v;
-        c.r = gray - static_cast<int>(gray * t);
-        c.g = gray - static_cast<int>(gray * t);
-        c.b = gray + static_cast<int>((255 - gray) * t);
-    }
-    c.a = 255;
+    unsigned char m = 255 - value * 255;
+
+    c = { m, m, m, 255 };
 
     return c;
 }
+
+//Color getColor(float value)
+//{
+//    const int gray = 128;
+//
+//    float v = value / 5.0f;
+//    if (v < -1.0f) v = -1.0f;
+//    if (v > 1.0f) v = 1.0f;
+//
+//    Color c;
+//
+//    if (v < 0.0f)
+//    {
+//        float t = -v;
+//        c.r = gray + static_cast<int>((255 - gray) * t);
+//        c.g = gray - static_cast<int>(gray * t);
+//        c.b = gray - static_cast<int>(gray * t);
+//    }
+//    else
+//    {
+//        float t = v;
+//        c.r = gray - static_cast<int>(gray * t);
+//        c.g = gray - static_cast<int>(gray * t);
+//        c.b = gray + static_cast<int>((255 - gray) * t);
+//    }
+//    c.a = 255;
+//
+//    return c;
+//}
